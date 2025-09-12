bb03fd3108cf6448482dc98696252aba0c7e5c82
kwen <kwen.zou@outlook.com>
2025-09-12 18:52:41 +0800

    ch9:structured logging and error handling

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,7 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/aikwen/greenlight/internal/data"
-	"log"
+	"github.com/aikwen/greenlight/internal/jsonlog"
 	"net/http"
 	"os"
 	"time"
@@ -31,7 +31,7 @@ type config struct {
 
 type application struct {
 	config config
-	logger *log.Logger
+	logger *jsonlog.Logger
 	models data.Models
 }
 
@@ -50,14 +50,14 @@ func main() {
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max idle time")
 	flag.Parse()
 
-	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
 
 	db, err := openDB(cfg)
 	if err != nil {
-		logger.Fatal(err)
+		logger.PrintFatal(err, nil)
 	}
 	defer db.Close()
-	logger.Printf("database connection established")
+	logger.PrintInfo("database connection established", nil)
 
 	// declare application
 	app := &application{
@@ -74,9 +74,13 @@ func main() {
 		WriteTimeout: 30 * time.Second,
 	}
 
-	logger.Printf("starting %s on port %d", cfg.env, cfg.port)
+	logger.PrintInfo("starting server", map[string]string{
+		"port": srv.Addr,
+		"env":  cfg.env,
+	})
+
 	err = srv.ListenAndServe()
-	logger.Fatal(err)
+	logger.PrintFatal(err, nil)
 }
 
 func openDB(cfg config) (*sql.DB, error) {
