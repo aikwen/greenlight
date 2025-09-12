13ec10e724dc8977b8e6ee9e9d5c38ba54f6847d
kwen <kwen.zou@outlook.com>
2025-09-12 23:40:55 +0800

    ch10:rate limiting

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/time/rate"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -14,6 +18,79 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
 			}
 		}()
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) rateLimit(next http.Handler) http.Handler {
+	// define a client struct to hold the rate limiter and
+	// last seen time for each client
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	// launch a background goroutine which removes old entries
+	// from the clients map once every time
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+
+			// lock the mutex to prevent any rate limiter
+			// checks from happening while the cleanup is taking
+			// place
+			mu.Lock()
+
+			// loop all clients
+			// if one haven's been seen within the last three
+			// minutes, delete the corresponding entry from the map
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+
+			// unlock the mutex when the cleanup is complete
+			mu.Unlock()
+		}
+	}()
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// if rate limiting is enabled carry out
+		if app.config.limiter.enabled {
+			ip, _, err := net.SplitHostPort(r.RemoteAddr)
+			if err != nil {
+				app.serverErrorResponse(w, r, err)
+				return
+			}
+
+			mu.Lock()
+
+			if _, found := clients[ip]; !found {
+				// if a client doesn't exist
+				// create and add it to the map
+				clients[ip] = &client{
+					limiter: rate.NewLimiter(rate.Limit(app.config.limiter.rps), app.config.limiter.burst),
+				}
+			}
+
+			// update the last seen time for the client
+			clients[ip].lastSeen = time.Now()
+
+			if !clients[ip].limiter.Allow() {
+				mu.Unlock()
+				app.rateLimitExceededResponse(w, r)
+				return
+			}
+
+			mu.Unlock()
+		}
+
 		next.ServeHTTP(w, r)
 	})
 }
