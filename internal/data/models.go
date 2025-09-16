99a16b548e49306b69b2cb67fb0166856ff57044
kwen <kwen.zou@outlook.com>
2025-09-16 16:50:14 +0800

    ch16:permission-base authorization

--- internal/data/models.go
+++ internal/data/models.go
@@ -11,15 +11,17 @@ var (
 )
 
 type Models struct {
-	Movies MovieModel
-	Tokens TokenModel
-	Users  UserModel
+	Movies      MovieModel
+	Permissions PermissionModel
+	Tokens      TokenModel
+	Users       UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
-		Movies: MovieModel{db},
-		Tokens: TokenModel{db},
-		Users:  UserModel{db},
+		Movies:      MovieModel{db},
+		Permissions: PermissionModel{db},
+		Tokens:      TokenModel{db},
+		Users:       UserModel{db},
 	}
 }
